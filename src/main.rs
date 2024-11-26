bb7b6512b15bfbf9a650126c2fa4204bc84f054a
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-11-26 21:08:28 +0000

    add env loading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use dotenvy::dotenv;
+use std::env;
+
+fn main() -> Result<()> {
+    dotenv()?;
+
+    let bot_token = env::var("BOT_TOKEN")?;
+    let queue_name = env::var("QUEUE_NAME")?;
+    let chat_id = env::var("CHAT_ID")?;
+
+    println!("{bot_token}, {queue_name}, {chat_id}");
+
+    Ok(())
 }
