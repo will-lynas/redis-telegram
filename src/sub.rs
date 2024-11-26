9f398148e825ee53782204de52fe138f7102421b
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-11-26 21:50:31 +0000

    send telegram messages

--- src/sub.rs
+++ src/sub.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
 use dotenvy::dotenv;
 use std::env;
+use teloxide::{prelude::Requester, Bot};
 use tokio_stream::StreamExt;
 
 #[tokio::main]
@@ -8,6 +9,7 @@ async fn main() -> Result<()> {
     dotenv()?;
 
     let channel = env::var("CHANNEL")?;
+    let chat_id = env::var("CHAT_ID")?;
 
     let client = redis::Client::open("redis://127.0.0.1/")?;
     let mut pubsub = client.get_async_pubsub().await?;
@@ -17,9 +19,12 @@ async fn main() -> Result<()> {
 
     let mut message_stream = pubsub.on_message();
 
+    let bot = Bot::from_env();
+
     while let Some(msg) = message_stream.next().await {
         let payload: String = msg.get_payload()?;
-        println!("Received: {}", payload);
+        println!("Received: {}", &payload);
+        bot.send_message(chat_id.clone(), payload).await?;
     }
 
     Ok(())
